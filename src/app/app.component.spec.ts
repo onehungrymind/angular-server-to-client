91e6d1c170ffeaf46166683f382c977c74dbbbac
Lukas Ruebbelke <simpul@gmail.com>
2017-05-11 06:26:17 -0700

    chore: updating dependencies and cleaning up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,12 @@
 
 import { TestBed, async } from '@angular/core/testing';
 
-import { RouterLinkStubDirective }   from './testing';
+import { RouterLinkStubDirective } from './testing';
 import { RouterOutletStubComponent } from './testing';
 
 import { AppComponent } from './app.component';
 
-describe('App: Ng2RestApp', () => {
+describe('App: Angular REST App', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -23,16 +23,16 @@ describe('App: Ng2RestApp', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have correct title`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular 2 REST Website');
+    expect(app.title).toEqual('Angular REST App');
   }));
 
   it('should render title in the header', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.mdl-layout-title').textContent).toContain('Angular 2 REST Website');
+    expect(compiled.querySelector('.mdl-layout-title').textContent).toContain('Angular REST App');
   }));
 });
