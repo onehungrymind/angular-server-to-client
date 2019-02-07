a9fb57d862b88be31f8c063401358f4c07dd13d1
Lukas Ruebbelke <simpul@gmail.com>
2019-02-07 07:02:25 -0700

    update: adding error handling

--- src/app/shared/items.service.ts
+++ src/app/shared/items.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Item } from './item.model';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 const BASE_URL = 'http://localhost:3000/items/';
 
@@ -9,7 +11,10 @@ export class ItemsService {
   constructor(private http: HttpClient) {}
 
   loadItems() {
-    return this.http.get(BASE_URL);
+    return this.http.get(BASE_URL)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   loadItem(id) {
@@ -31,4 +36,18 @@ export class ItemsService {
   deleteItem(item: Item) {
     return this.http.delete(`${BASE_URL}${item.id}`);
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error('Error Status: ', error.status);
+      console.error('Error Details: ', error.error);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  };
 }
